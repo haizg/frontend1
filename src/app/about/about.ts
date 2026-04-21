6dc21c5b17a50194780521549a9a0b28812d67f5
haizg <haifazghondi20@gmail.com>
2026-04-21 21:10:06 +0100

    .

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -4,10 +4,11 @@ import { RouterModule } from '@angular/router';
 import { Navbar } from '../navbar/navbar';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslateLangService } from '../services/translate-lang.service';
+import { Footer } from '../shared/footer/footer';
 
 @Component({
   selector: 'app-about',
-  imports: [CommonModule, RouterModule, Navbar, TranslateModule],
+  imports: [CommonModule, RouterModule, Navbar, TranslateModule, Footer],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
